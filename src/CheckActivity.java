5a03b7acf8aa9643550fa7ad110c0c5be5c51c95
Vineet Agrawal <vinekua1@in.ibm.com>
2020-05-13 20:34:33 +0530

    Updated Comments & added conditions checks

--- src/CheckActivity.java
+++ src/CheckActivity.java
@@ -1,13 +1,18 @@
 public class CheckActivity {
 
+    //find suspicious accounts
     public String[][] flagAccount(String[][] customerData) {
 
+        //2D array to simulate database
+        //one person with multiple accounts can be stored
         String suspiciousAccount[][] = new String[100][100];
         int row = 0, col = 0, flag = 0;
 
         for (int i = 1; i < 4; i++) {
             for (int j = i + 1; j <= 5; j++) {
+                //condition to find same person with multiple account
                 if (customerData[i][2] == customerData[j][2] && customerData[i][3] == customerData[j][3]) {
+                    //check same account is not duplicated
                     if (flag == 0) {
                         suspiciousAccount[row][col] = customerData[i][0];
                         flag = 1;
@@ -21,11 +26,15 @@ public class CheckActivity {
         return suspiciousAccount;
     }
 
+    //find suspicious transactions
     public String[] flagTransaction(String transactionData[][] ,String suspiciousAccount[][])
     {
+        //array to contains all suspicious/flagged transaction
+        //initial size kept at 100, can be scaled.
         String suspiciousTransaction[] = new String[100];
         int count = 0;
 
+        //check if suspicious accounts present
         if(suspiciousAccount[0][0] == null)
         {
             System.out.println("No Suspicious Transaction Occured");
@@ -33,10 +42,13 @@ public class CheckActivity {
         else {
             for (int i = 0; i < 2; i++) {
                 for (int j = 1; j < 100; j++) {
+                    //same person to same person transaction condition 1
                     if ((suspiciousAccount[i][0] == transactionData[j][2]) && (suspiciousAccount[i][1] == transactionData[j][3])) {
                         suspiciousTransaction[count] = transactionData[j][0];
                         count += 1;
-                    } else if ((suspiciousAccount[i][1] == transactionData[j][2]) && (suspiciousAccount[i][0] == transactionData[j][3])) {
+                    }
+                    //same person to same person transaction condition 2
+                    else if ((suspiciousAccount[i][1] == transactionData[j][2]) && (suspiciousAccount[i][0] == transactionData[j][3])) {
                         suspiciousTransaction[count] = transactionData[j][0];
                         count += 1;
                     } else
--- src/GenerateReport.java
+++ src/GenerateReport.java
@@ -1,71 +1,81 @@
 public class GenerateReport {
 
+    //Generate Report for Suspicious Transaction
     public void suspiciousTransactionReport(String suspiciousTransaction[], String transactionData[][])
     {
-        String month[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        //Check if any Suspicious transaction present
+        if (suspiciousTransaction[0] == null) {
+            System.out.println("No Suspicious Transaction!");
+        } else {
+            //String array simulate a calendar
+            String month[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-        String groupByMonth[][] = new String[12][100];
+            //Group/Sort all suspicious transaction by Month
+            String groupByMonth[][] = new String[12][100];
 
-        for (int i=0; i<12; i++)
-        {
-            groupByMonth[i][0] = month[i];
-        }
+            for (int i = 0; i < 12; i++) {
+                groupByMonth[i][0] = month[i];
+            }
 
-        int subCharPosition = 3, count = 1;
+            int subCharPosition = 3, count = 1;
 
-        for(int i=1; i<15; i++)
-        {
-            for(int j=0; j<suspiciousTransaction.length; j++) {
-                if (transactionData[i][0] == suspiciousTransaction[j])
-                {
-                    int len = transactionData[i][1].length();
-                    if (len==8)
-                        subCharPosition-=1;
+            for (int i = 1; i < 15; i++) {
+                for (int j = 0; j < suspiciousTransaction.length; j++) {
+                    //Double check if the suspicious transaction is present in database
+                    if (transactionData[i][0] == suspiciousTransaction[j]) {
+                        int len = transactionData[i][1].length();
+                        if (len == 8)
+                            subCharPosition -= 1;
 
-                    String transMonth = transactionData[i][1].substring(subCharPosition,subCharPosition+3);
+                        //fetch the transaction Month from details
+                        String transMonth = transactionData[i][1].substring(subCharPosition, subCharPosition + 3);
 
-                    for(int k=0; k<12;k++)
-                    {
-                        if(transMonth.equals(groupByMonth[k][0]))
-                        {
-                            while(groupByMonth[k][count]!=null)
-                                count++;
+                        //Sort all the suspicious transaction by month
+                        for (int k = 0; k < 12; k++) {
+                            if (transMonth.equals(groupByMonth[k][0])) {
+                                while (groupByMonth[k][count] != null)
+                                    count++;
 
-                            groupByMonth[k][count] = suspiciousTransaction[j];
+                                groupByMonth[k][count] = suspiciousTransaction[j];
+                            }
                         }
+                        break;
                     }
-                    break;
+                    subCharPosition = 3;
+                    count = 0;
                 }
-                subCharPosition = 3; count = 0;
             }
-        }
 
-        count = 1;
-
-        for (int i=0; i<12; i++)
-        {
-            if(groupByMonth[i][count] != null) {
-                System.out.println("For the Month of "+month[i]);
-                System.out.println("Suspicious Transactions :");
-                while (groupByMonth[i][count] != null)
-                {
-                    System.out.println(groupByMonth[i][count]);
-                    count++;
-                }
-                System.out.println();
-            }
             count = 1;
+
+            //Generate Report
+            for (int i = 0; i < 12; i++) {
+                //check if any Month has no suspicious transactions
+                if (groupByMonth[i][count] != null) {
+                    System.out.println("For the Month of " + month[i]);
+                    System.out.println("Suspicious Transactions :");
+                    while (groupByMonth[i][count] != null) {
+                        System.out.println(groupByMonth[i][count]);
+                        count++;
+                    }
+                    System.out.println();
+                }
+                count = 1;
+            }
         }
     }
 
+    //Generate Report for Suspicious Accounts
     public void suspiciousAccountReport(String[][] suspiciousAccount)
     {
         int flag = 0;
 
+        //check if any suspicious account is present
         if (suspiciousAccount[0][0] == null)
-            System.out.println("No Suspicious Accounts present");
+            System.out.println("No Suspicious Accounts present!");
         else {
             System.out.println("Suspicious Accounts");
+
             for (int i = 0; i < 100; i++) {
                 for (int j = 0; j < 2; j++) {
                     if (suspiciousAccount[i][j] == null) {
